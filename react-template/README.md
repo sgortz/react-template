b57fddadb095cbd457e3a279656b7b5439b314bd
Sabrina Gortz <sabrina.gortz@gmail.com>
2022-05-02 14:36:40 -0400

    Initial Commit

---
+++ react-template/README.md
@@ -0,0 +1,4 @@
+# This is a Template for a React Application
+To get started type in the Terminal:
+```npm install
+``` 
---
+++ react-template/webpack.config.js
@@ -0,0 +1,45 @@
+var path = require('path');
+var SRC_DIR = path.join(__dirname, '/client/src');
+var DIST_DIR = path.join(__dirname, '/client/dist');
+
+module.exports = {
+  entry: `${SRC_DIR}/index.jsx`,
+  output: {
+    filename: 'bundle.js',
+    path: DIST_DIR
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)?/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              "@babel/preset-env",
+              "@babel/preset-react"
+            ],
+            plugins: [
+              ["@babel/plugin-transform-runtime",
+                {
+                  "regenerator": true
+                }
+              ]
+            ]
+          }
+        }
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.(jpg|png)$/,
+        use: {
+          loader: 'url-loader',
+        },
+      }
+    ]
+  }
+};
