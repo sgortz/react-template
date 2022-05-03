52d3b439c4f84997dbac007e214fc0f44db526a5
Sabrina Gortz <sabrina.gortz@gmail.com>
2022-05-02 20:38:29 -0400

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -5,26 +5,26 @@ var DIST_DIR = path.join(__dirname, '/client/dist');
 module.exports = {
   // "development" will be human readable 
   // "production" will be minified
-  mode: "development",
-  entry: `${SRC_DIR}/index.jsx`,
-  output: {
-    filename: 'bundle.js',
-    path: DIST_DIR
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)?/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: [
-              "@babel/preset-env",
-              "@babel/preset-react"
-            ],
-            plugins: [
-              ["@babel/plugin-transform-runtime",
+    mode: "development",
+    entry: `${SRC_DIR}/index.js`,
+    output: {
+      filename: 'bundle.js',
+      path: DIST_DIR
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: [
+                "@babel/preset-env",
+                "@babel/preset-react"
+              ],
+              plugins: [
+                ["@babel/plugin-transform-runtime",
                 {
                   "regenerator": true
                 }
@@ -42,6 +42,15 @@ module.exports = {
         use: {
           loader: 'url-loader',
         },
+        devtool: "eval-cheap-module-source-map",
+        // [devServer] configuration for the live server including port
+        devServer: {
+        // [static] config for how what to serve
+        static: {
+          directory: path.join(__dirname, 'dist'),
+        },
+        compress: true,
+        port: 3000,
       }
     ]
   }
