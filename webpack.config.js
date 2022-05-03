51ad23a97c794c7c9973403cca23e2bd4621a842
Sabrina Gortz <sabrina.gortz@gmail.com>
2022-05-02 20:52:38 -0400

    Fixed bug on webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -3,19 +3,48 @@ var SRC_DIR = path.join(__dirname, '/client/src');
 var DIST_DIR = path.join(__dirname, '/client/dist');
 
 module.exports = {
-  // "development" will be human readable 
-  // "production" will be minified
-    mode: "development",
-    entry: `${SRC_DIR}/index.js`,
-    output: {
-      filename: 'bundle.js',
-      path: DIST_DIR
-    },
-    module: {
-      rules: [
-        {
-          test: /\.(js|jsx)$/,
-          exclude: /node_modules/,
+     // "development" will be human readable 
+     // "production" will be minified
+     mode: "development",
+     entry: `${SRC_DIR}/index.js`,
+     output: {
+          filename: 'bundle.js',
+          path: DIST_DIR
+     },
+     module: {
+          rules: [
+               {
+                    test: /\.(jsx|js)$/,
+                    exclude: /node_modules/,
+                    loader: "babel-loader"
+               },
+               {
+                    test: /\.css$/,
+                    use: ['style-loader', 'css-loader']
+               },
+               {
+                    test: /\.(jpg|png)$/,
+                    use: {
+                         loader: 'url-loader',
+                    }
+               }
+          ]
+     },
+     devtool: "eval-cheap-module-source-map",
+     // [devServer] configuration for the live server including port
+     devServer: {
+          // [static] config for how what to serve
+          static: {
+               directory: DIST_DIR,
+          },
+          compress: true,
+          port: 3000,
+     }
+};
+
+
+/*
+CHANGE LINE 19 FOR:
           use: {
             loader: "babel-loader",
             options: {
@@ -33,25 +62,4 @@ module.exports = {
           }
         }
       },
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"]
-      },
-      {
-        test: /\.(jpg|png)$/,
-        use: {
-          loader: 'url-loader',
-        },
-        devtool: "eval-cheap-module-source-map",
-        // [devServer] configuration for the live server including port
-        devServer: {
-        // [static] config for how what to serve
-        static: {
-          directory: path.join(__dirname, 'dist'),
-        },
-        compress: true,
-        port: 3000,
-      }
-    ]
-  }
-};
+*/
